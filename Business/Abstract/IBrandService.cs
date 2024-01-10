ac57fee360f94729a5736ddd393db74b3f45e6df
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-01-10 19:45:10 +0300

    Business Layer was added.

---
+++ Business/Abstract/IBrandService.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Concretes;
+
+namespace Business.Abstract
+{
+    internal interface IBrandService
+    {
+        public Brand Add(Brand brand);
+        public IList<Brand> GetList();
+    }
+}
---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concretes;
+using System.Collections.Generic;
+namespace Business.Concretes
+{
+    internal class BrandManager : IBrandService
+    {
+        private IBrandDal _brandDal;
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+       
+        public Brand Add(Brand brand)
+        {
+            _brandDal.Add(brand);
+            return brand;
+        }
+
+        public IList<Brand> GetList()
+        {
+            IList<Brand> brandList = _brandDal.GetList();
+            return brandList;
+        }
+    }
+}
